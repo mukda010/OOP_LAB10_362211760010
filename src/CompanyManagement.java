f342bf3b3fcbb0ac9805885b81df233335a981e8
mukda <kiwmuk456@gmail.com>
2021-03-16 11:48:09 +0700

    update

--- src/CompanyManagement.java
+++ src/CompanyManagement.java
@@ -2,26 +2,38 @@ import java.util.List;
 
 public class CompanyManagement {
     public static void main(String[] args) {
-
         //Create instance
-
         EmployeeDAOImpl dao = EmployeeDAOImpl.getInstance();
 
-        //dispaly all employee data
-        displayAllEmp(dao);
-        //add new
+        //display all employee data
+        //displayAllEmp(dao);
 
+        //add new employee
+        //addNewEmp(dao);
 
+        findEmpByID(dao);
 
+    }//main
 
+    private static void findEmpByID(EmployeeDAOImpl dao) {
+        Employee emp = dao.findById("EMP005");
+        if (emp != null){
+            System.out.println(emp.toString());
+        }
+    }
 
+    private static void addNewEmp(EmployeeDAOImpl dao) {
+        Employee myEmp = new Employee("EMP005","JD BC","Student","JD@gmail.com",50000);
+        dao.addEmp(myEmp);
     }
 
     private static void displayAllEmp(EmployeeDAOImpl dao) {
-         List<Employee> emp = dao.getAllEmp();
-         System.out.println("Employee Data:");
-         for (Employee e:emp){
-             System.out.println(e.toString());
-         }
+        List<Employee> emp = dao.getAllEmp();
+        System.out.println("Employee Data : ");
+        for (Employee e: emp) {
+            System.out.println(e.toString());
+        }//for
+
     }
-}
+
+}//class
